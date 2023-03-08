a7f920e04fa509ed08f745492f61ea40e37cc8b2
Jefferson Lopes <dev.jefferson.lopes@gmail.com>
2023-03-08 10:24:14 -0300

    criando paginas e rotas

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import Home from './pages/Home';
+import Filme from './pages/Filme';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/filme/:id",
+    element: <Filme />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+       <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/pages/Filme/index.jsx
@@ -0,0 +1,9 @@
+const Filme = () => {
+    return (
+      <div>
+        <h1>Bem vindo a página detalhes do filme</h1>
+      </div>
+    )
+  }
+  
+  export default Filme
