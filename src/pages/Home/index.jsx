5e13ac3f86378599b453cad9d9a21c6118b62013
Jefferson Lopes <dev.jefferson.lopes@gmail.com>
2023-03-08 22:23:14 -0300

    Instalando axios e configurando API

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,27 @@
+import {useEffect, useState} from 'react'
+import api from '../../services/api'
+
 const Home = () => {
+  const [filmes, setFilmes] = useState([])
+
+  useEffect(()=>{
+
+    async function loadFilmes(){
+      const response = await api.get("/movie/now_playing", {
+        params:{
+          api_key: "d74ad846ba48ac20c45716dc0014b14f",
+          language: "pt-br",
+          page: 1,
+        }
+      })
+
+      console.log(response.data.results)
+    }
+
+    loadFilmes()
+
+  }, [])
+
   return (
     <div>
         <h1>Bem vindo a Home</h1>
